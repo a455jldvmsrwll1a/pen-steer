1c87f8275e94cc33e6a45cbe8a49692b4b9fd390
a455jldvmsrwll1a <51287554+a455jldvmsrwll1a@users.noreply.github.com>
2025-07-22 01:45:21 -0500

    Network source done-ish

--- src/controller.rs
+++ src/controller.rs
@@ -1,11 +1,36 @@
-use std::sync::{Arc, Mutex};
+use std::{sync::{Arc, Mutex}, thread, time::Duration};
+use anyhow::Result;
 
-use crate::{state::State, timer::Timer};
+use crate::{config, source::{net::NetSource, Source}, state::State, timer::Timer};
 
 pub fn controller(state: Arc<Mutex<State>>) {
     let mut timer = Timer::new(state.lock().unwrap().config.update_frequency);
 
     loop {
+        if let Err(err) = update(&mut state.lock().unwrap()) {
+            eprintln!("Controller error: {err}");
+        }
+
         timer.wait();
     }
+}
+
+pub fn update(state: &mut State) -> Result<()> {
+    if state.outdated {
+        initialise_io(state)?;
+    }
+
+    Ok(())
+}
+
+pub fn initialise_io(state: &mut State) -> Result<()> {
+    state.source = match state.config.source {
+        config::Source::None => Source::Dummy,
+        config::Source::Net => Source::Net(NetSource::new(&state.config.net_sock_addr)?),
+        config::Source::Wintab => todo!(),
+    };
+
+    state.outdated = false;
+
+    Ok(())
 }
