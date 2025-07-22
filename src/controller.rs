c015f867b564e7601ff3bea408b7ffe6bfe33a91
Jann Atienza <minelolpjl@gmail.com>
2025-07-22 14:10:10 -0500

    Allow dragging the steering wheel via the view.

--- src/controller.rs
+++ src/controller.rs
@@ -1,7 +1,12 @@
-use std::sync::{Arc, Mutex};
 use anyhow::Result;
+use std::sync::{Arc, Mutex};
 
-use crate::{config, source::{net::NetSource, Source}, state::State, timer::Timer};
+use crate::{
+    config,
+    source::{Source, net::NetSource},
+    state::State,
+    timer::Timer,
+};
 
 pub fn controller(state: Arc<Mutex<State>>) {
     let mut timer = Timer::new(state.lock().unwrap().config.update_frequency);
@@ -20,8 +25,15 @@ pub fn update(state: &mut State) -> Result<()> {
         initialise_io(state)?;
     }
 
-    state.pen = state.source.get();
-    state.wheel.update(&state.config, state.pen.clone(), 1.0 / state.config.update_frequency as f32);
+    if let Some(ref pen) = state.source.get() {
+        state.pen = Some(pen.clone());
+    }
+
+    state.wheel.update(
+        &state.config,
+        state.pen_override.clone().or_else(|| state.pen.clone()),
+        1.0 / state.config.update_frequency as f32,
+    );
 
     Ok(())
 }
