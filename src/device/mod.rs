baa1bf61916ded19ebbbe739e8a2ff65ab87e84e
a455jldvmsrwll1a <51287554+a455jldvmsrwll1a@users.noreply.github.com>
2025-07-22 17:28:53 -0500

    Output wheel state to device

--- src/device/mod.rs
+++ src/device/mod.rs
@@ -4,6 +4,8 @@ pub mod uinput;
 #[cfg(target_os = "linux")]
 use crate::device::uinput::UInputDev;
 
+use anyhow::Result;
+
 #[derive(Debug, Default)]
 pub enum Device {
     /// Dummy device, does nothing.
@@ -12,4 +14,30 @@ pub enum Device {
     /// Presents a virtual device using Linux's uinput.
     #[cfg(target_os = "linux")]
     UInput(UInputDev),
+}
+
+impl Device {
+    pub fn set_wheel(&mut self, angle: f32) {
+        match self {
+            Device::Dummy => (),
+            #[cfg(target_os = "linux")]
+            Device::UInput(uinput_dev) => uinput_dev.set_wheel(angle),
+        }
+    }
+
+    pub fn set_horn(&mut self, honking: bool) {
+        match self {
+            Device::Dummy => (),
+            #[cfg(target_os = "linux")]
+            Device::UInput(uinput_dev) => uinput_dev.set_horn(honking),
+        }
+    }
+
+    pub fn apply(&mut self) -> Result<()> {
+        match self {
+            Device::Dummy => Ok(()),
+            #[cfg(target_os = "linux")]
+            Device::UInput(uinput_dev) => uinput_dev.apply(),
+        }
+    }
 }
