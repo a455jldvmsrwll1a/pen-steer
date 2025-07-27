f69ad8bede33774e0f81fc381d80ef766e9dc6c8
a455jldvmsrwll1a <51287554+a455jldvmsrwll1a@users.noreply.github.com>
2025-07-26 21:00:57 -0500

    Implement Mapping::transform().

--- src/mapping.rs
+++ src/mapping.rs
@@ -40,7 +40,34 @@ impl Default for Mapping {
 }
 
 impl Mapping {
-    pub fn transform(&self, x: f32, y: f32) -> (f32, f32) {
-        (x, y)
+    pub fn transform(&self, mut x: f32, mut y: f32) -> (f32, f32) {
+        x = inv_lerp(x, self.min_in_x, self.max_in_x).clamp(0.0, 1.0);
+        y = inv_lerp(y, self.min_in_y, self.max_in_y).clamp(0.0, 1.0);
+
+        if self.invert_x {
+            x = 1.0 - x;
+        }
+
+        if self.invert_y {
+            y = 1.0 - y;
+        }
+
+        x = lerp(x, self.min_out_x, self.max_out_x).clamp(-1.0, 1.0);
+        y = lerp(y, self.min_out_y, self.max_out_y).clamp(-1.0, 1.0);
+
+        match self.orientation {
+            MapOrientation::None => (x, y),
+            MapOrientation::A90 => (-y, x),
+            MapOrientation::A180 => (-x, -y),
+            MapOrientation::A270 => (y, -x),
+        }
     }
+}
+
+fn lerp(t: f32, b1: f32, b2: f32) -> f32 {
+    b1 + t * (b2 - b1)
+}
+
+fn inv_lerp(t: f32, a1: f32, a2: f32) -> f32 {
+    (t - a1) / (a2 - a1)
 }
