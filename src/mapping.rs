e80a3db2eba36877fbc8a479fc9ad99508c2f914
a455jldvmsrwll1a <51287554+a455jldvmsrwll1a@users.noreply.github.com>
2025-07-26 21:28:11 -0500

    Implement save/load for Mapping struct.

--- src/mapping.rs
+++ src/mapping.rs
@@ -10,17 +10,17 @@ pub enum MapOrientation {
 
 #[derive(Debug, Clone)]
 pub struct Mapping {
-    min_in_x: f32,
-    min_in_y: f32,
-    max_in_x: f32,
-    max_in_y: f32,
-    min_out_x: f32,
-    min_out_y: f32,
-    max_out_x: f32,
-    max_out_y: f32,
-    orientation: MapOrientation,
-    invert_x: bool,
-    invert_y: bool,
+    pub min_in_x: f32,
+    pub min_in_y: f32,
+    pub max_in_x: f32,
+    pub max_in_y: f32,
+    pub min_out_x: f32,
+    pub min_out_y: f32,
+    pub max_out_x: f32,
+    pub max_out_y: f32,
+    pub orientation: MapOrientation,
+    pub invert_x: bool,
+    pub invert_y: bool,
 }
 
 impl Default for Mapping {
