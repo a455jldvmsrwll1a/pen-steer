df99c864905582900607cdbdc0a77d3e1beb04e8
Jann Atienza <minelolpjl@gmail.com>
2025-07-26 21:01:55 -0500

    Add RawPen struct.
    RawPen will hold arbitrary-ish coordinates, while Pen will always have [-1, 1].

--- src/pen.rs
+++ src/pen.rs
@@ -5,3 +5,11 @@ pub struct Pen {
     pub pressure: u32,
     pub buttons: u8,
 }
+
+#[derive(Debug, Default, Clone)]
+pub struct RawPen {
+    pub x: f32,
+    pub y: f32,
+    pub pressure: u32,
+    pub buttons: u8,
+}
