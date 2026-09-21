04ac784aa17105e7aab4edc2e62ac87173fd3ceb
jl <51287554+a455jldvmsrwll1a@users.noreply.github.com>
2026-09-21 16:59:18 -0500

    some comments

--- src/device/mod.rs
+++ src/device/mod.rs
@@ -12,19 +12,51 @@ use crate::device::vigem::VigemDevice;
 
 use anyhow::Result;
 
+/// Backend for the output device.
+/// 
 pub trait Device: Send + Sync {
+    /// Get the current feedback force from the output device.
+    /// 
+    /// # Returns
+    /// 
+    /// The feedback force (in no particular unit), or `None` if unavailable:
+    /// 
+    /// - The output backend or device does not support force-feedback.
+    /// 
+    /// - Force-feedback is currently not being used by any process.
+    /// 
     fn get_feedback(&self) -> Option<f32>;
 
+    /// Set the wheel axis value.
+    /// 
+    /// Full range is [-1.0, 1.0], in no particular unit.
+    /// 
     fn set_wheel(&mut self, angle: f32);
 
+    /// Set the accelerator axis value.
+    /// 
+    /// Full range is [0.0, 1.0], in no particular unit.
+    /// 
     fn set_accelerator(&mut self, normalised: f32);
 
+    /// Set the brake axis value.
+    /// 
+    /// Full range is [0.0, 1.0], in no particular unit.
+    /// 
     fn set_brake(&mut self, normalised: f32);
 
+    /// Set the horn's state.
+    /// 
+    /// `true` if the horn is pressed, and `false` if not.
+    /// 
     fn set_horn(&mut self, honking: bool);
 
+    /// Apply any changes that were made to the actual output mechanism (e.g. uinput or vigem).
+    /// 
     fn apply(&mut self) -> Result<()>;
 
+    /// Handle any events that the backend needs to respond to (such as receiving force-feedback events).
+    /// 
     fn handle_events(&mut self);
 }
 
